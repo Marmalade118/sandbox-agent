3719aec53f4c1e512e3df0a9c5f6c4ec2a759f88
Nathan Flurry <developer@nathanflurry.com>
2026-03-09 19:59:32 -0700

    chore: recover delhi-v1 workspace state

--- sdks/react/src/index.ts
+++ sdks/react/src/index.ts
@@ -1,4 +1,10 @@
 export { ProcessTerminal } from "./ProcessTerminal.tsx";
+export { DesktopViewer } from "./DesktopViewer.tsx";
+
+export type {
+  DesktopViewerClient,
+  DesktopViewerProps,
+} from "./DesktopViewer.tsx";
 
 export type {
   ProcessTerminalClient,
